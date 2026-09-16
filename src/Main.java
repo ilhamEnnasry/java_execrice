d01d232ab1793981db850ed2d7b131d117733887
ENNASRY ILHAM <ilhamennasry@gmail.com>
2026-09-16 18:24:07 +0100

    Excerice in java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,3 @@
-import java.math.BigDecimal;
-import java.text.NumberFormat;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
